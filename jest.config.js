41c8f3c550efa7b3a51897dc78e33bd3deab4c6c
HugoNicolau <nicolau.hugogiles@gmail.com>
2023-02-24 21:11:40 -0300

    fix: Run in band jest configuration

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
     moduleDirectories: ["node_modules", "src"],
+    runInBand: true,
     transform: {
       ".+\\.ts$": "ts-jest",
     },
